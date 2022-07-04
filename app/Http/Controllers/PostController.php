7d2d10d255b8edab37553d140aa2f803bf556824
tatitan <tetsuef24@gmail.com>
2022-07-04 23:25:25 +0900

    投稿内容の制限、精査など

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Post;
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
@@ -21,5 +21,13 @@ class PostController extends Controller
     public function create()
     {
         return view('posts/create');
+    }
+
+    public function store(Post $post, PostRequest $request)
+    {
+        //dd($request->all());
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ Route::get('/', 'PostController@index');
 Route::get('/posts/create', 'PostController@create');
 Route::get('/posts/{post}', 'PostController@show');
 
+Route::post('/posts', 'PostController@store');
 
 /*Route::get('/', function() {
     return view('posts/index');
