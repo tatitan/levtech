bbace3c81bdc4c7bd93c8592cb4c2cdeccda11fd
tatitan <tetsuef24@gmail.com>
2022-07-10 14:26:59 +0900

    08-5 edit

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,6 +8,7 @@
     </head>
     <body>
         <h1>Blog Name</h1>
+        <p class='edit'>[<a href="/posts/{{$post->id}}/edit">edit</a>]</p>
         <div crass='post'>
             <h2 class='title'>{{ $post->title }}</h2>
             <p class='body'>{{ $post->body }}</p>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'PostController@index');
 Route::get('/posts/create', 'PostController@create');
 Route::get('/posts/{post}', 'PostController@show');
-
+Route::get('/posts/{post}/edit', 'PostController@edit');
+Route::put('/posts/{post}', 'PostController@update');
 Route::post('/posts', 'PostController@store');
 
 /*Route::get('/', function() {
