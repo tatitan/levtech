800dad6bad817b3e4528bba09d068098f894505c
tatitan <tetsuef24@gmail.com>
2022-07-11 00:37:31 +0900

    08-6 delete

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'PostController@index');
 Route::get('/posts/create', 'PostController@create');
-Route::get('/posts/{post}', 'PostController@show');
 Route::get('/posts/{post}/edit', 'PostController@edit');
 Route::put('/posts/{post}', 'PostController@update');
+Route::delete('/posts/{post}', 'PostController@destroy');
+Route::get('/posts/{post}', 'PostController@show');
 Route::post('/posts', 'PostController@store');
+
+
 
 /*Route::get('/', function() {
     return view('posts/index');
