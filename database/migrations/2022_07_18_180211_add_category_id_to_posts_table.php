525799a28c1399bfe2901b13920df0f8d1ce16ca
tatitan <tetsuef24@gmail.com>
2022-07-19 21:57:38 +0900

    リレーション

--- database/migrations/2022_07_18_180211_add_category_id_to_posts_table.php
+++ database/migrations/2022_07_18_180211_add_category_id_to_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+class AddCategoryIdToPostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,9 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
+        Schema::table('posts', function (Blueprint $table) {
+            $table->integer('category_id')->unsigned();
+            //
         });
     }
 
@@ -26,6 +26,8 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
     }
 }
